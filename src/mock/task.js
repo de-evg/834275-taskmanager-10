f8eda4fb4ccc08f9fdc5012e3e29a0baca474438
de-evg <d.e.min@yandex.ru>
2019-11-30 11:41:40 +0300

    task не доделано

---
+++ src/mock/task.js
@@ -0,0 +1,23 @@
+const Descriptions = [
+  `Изучить теорию`,
+  `Сделать домашку`,
+  `Пройти интенсив на соточку`
+];
+
+const ReapetingDays = {
+  'mo': false,
+  'tu': false,
+  'we': false,
+  'th': false,
+  'fr': false,
+  'sa': false,
+  'su': false
+};
+
+const Tags = [
+  `homework`,
+  `theory`,
+  `practice`,
+  `intensive`,
+  `
+]
