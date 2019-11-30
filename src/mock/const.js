cfb84d2a1c854154e039fc1ba59d3c838a39d96d
de-evg <d.e.min@yandex.ru>
2019-11-30 12:39:41 +0300

    добавил вспомогательные данные и функции формирования времени

--- src/mock/const.js
+++ src/mock/const.js
@@ -1,7 +1,34 @@
-export const Colors = [
+const Colors = [
   `black`,
   `yellow`,
   `blue`,
   `green`,
   `pink`
 ];
+
+const Days = [
+  `mo`,
+  `tu`,
+  `we`,
+  `th`,
+  `fr`,
+  `sa`,
+  `su`
+];
+
+const MonthNames = [
+  `January`,
+  `February`,
+  `March`,
+  `April`,
+  `May`,
+  `June`,
+  `July`,
+  `August`,
+  `September`,
+  `October`,
+  `November`,
+  `December`,
+];
+
+export {Colors, Days, MonthNames};
